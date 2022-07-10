f0e5bb83b6035b52d3dc1d12ccde39537c8408d0
apudo-Sebby <sebbyapudo@gmail.com>
2022-07-11 00:16:31 +0300

    Finished setting up the scroll-up button that appears on scroll once the height exceeds the initial viewport height

--- app.js
+++ app.js
@@ -38,6 +38,13 @@ window.addEventListener('scroll', () => {
    else {
       navbar.classList.remove('fixed-nav');
    }
+
+   if(scrollHeight > 500) {
+      topLink.classList.add('show-link');
+   }
+   else {
+      topLink.classList.remove('show-link')
+   }
 });
 
 // ********** smooth scroll ************
